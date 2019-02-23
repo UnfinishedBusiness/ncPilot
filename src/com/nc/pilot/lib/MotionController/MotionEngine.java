e858912335792fe91ef6322058ed68e2cf4509a3
UnfinishedBusiness <travis.gillin@gmail.com>
2019-02-23 01:13:53 -0500

    Arc Interpolation now works

--- src/com/nc/pilot/lib/MotionController/MotionEngine.java
+++ src/com/nc/pilot/lib/MotionController/MotionEngine.java
@@ -41,6 +41,121 @@ public class MotionEngine {
     {
         return System.nanoTime() / 500;
     }
+    private static boolean inTolerance(float a, float b, float t)
+    {
+        float diff;
+        if (a > b)
+        {
+            diff = a - b;
+        }
+        else
+        {
+            diff = b - a;
+        }
+        if (diff <= Math.abs(t) && diff >= -Math.abs(t))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static float getAngle(float[] start_point, float[] end_point) {
+        float angle = (float) Math.toDegrees(Math.atan2(start_point[1] - end_point[1], start_point[0] - end_point[0]));
+
+        angle += 180;
+        if(angle >= 360){
+            angle -= 360;
+        }
+        if(angle < 0){
+            angle += 360;
+        }
+
+        return angle;
+    }
+    public static float getLineLength(float[] start_point, float[] end_point)
+    {
+        return new Float(Math.hypot(start_point[0]-end_point[0], start_point[1]-end_point[1]));
+    }
+    public static float[] rotatePoint(float[] pivot, float[] rotated_point, float angle)
+    {
+        float s = (float)Math.sin(angle*Math.PI/180);
+        float c = (float)Math.cos(angle*Math.PI/180);
+
+        // translate point back to origin:
+        rotated_point[0] -= pivot[0];
+        rotated_point[1] -= pivot[1];
+
+        // rotate point
+        float xnew = (rotated_point[0] * c - rotated_point[1] * s);
+        float ynew = (rotated_point[0] * s + rotated_point[1] * c);
+
+        // translate point back:
+        rotated_point[0] = xnew + pivot[0];
+        rotated_point[1] = ynew + pivot[1];
+        return new float[] {rotated_point[0], rotated_point[1]};
+    }
+    public static float[] getPolarLineEndpoint(float[] start_point, float length, float angle)
+    {
+        float[] end_point = new float[] {start_point[0] + length, start_point[1]};
+        return rotatePoint(start_point, end_point, angle);
+    }
+    public static ArrayList<float[]> getPointsOfArc(float[] start, float[] end, float[] center, float radius, String direction, int angle_inc)
+    {
+        float start_angle = getAngle(center, start);
+        float end_angle = getAngle(center, end);
+        ArrayList<float[]> points = new ArrayList();
+        points.add(start);
+        //System.out.println("start_angle: " + start_angle + " end_angle: " + end_angle);
+        if (start_angle == end_angle) //We are a circle
+        {
+            for (float x = 0; x < 360; x += angle_inc)
+            {
+                start_angle += angle_inc;
+                float [] new_point = getPolarLineEndpoint(center, radius, start_angle);
+                points.add(new_point);
+            }
+        }
+        else
+        {
+            if (direction == "CW")
+            {
+                for (int x = 0; x < 400; x++) //Runaway protection!
+                {
+                    start_angle -= angle_inc;
+                    if (start_angle <= 0)
+                    {
+                        start_angle = 360;
+                    }
+                    else if (inTolerance(start_angle, end_angle, angle_inc * 2))
+                    {
+                        break; //End of arc, break loop!
+                    }
+                    float [] new_point = getPolarLineEndpoint(center, radius, start_angle);
+                    points.add(new_point);
+                }
+            }
+            else
+            {
+                for (int x = 0; x < 400; x++) //Runaway protection!
+                {
+                    start_angle += angle_inc;
+                    if (start_angle >= 360)
+                    {
+                        start_angle = 0;
+                    }
+                    else if (inTolerance(start_angle, end_angle, angle_inc * 2)) break; //End of arc, break loop!
+                    float [] new_point = getPolarLineEndpoint(center, radius, start_angle);
+                    points.add(new_point);
+                }
+            }
+            //float [] new_point = getPolarLineEndpoint(center, radius, end_angle);
+            //points.add(new_point);
+            points.add(end);
+        }
+        return points;
+    }
     public float distanceBetween3DPoints(float[] p1, float[] p2)
     {
         return (float) Math.sqrt(Math.pow(p1[0] - p2[0], 2) + Math.pow(p1[1] - p2[1], 2) + Math.pow(p1[2] - p2[2], 2));
@@ -55,6 +170,10 @@ public class MotionEngine {
         x_feed = (long)(move_will_take / ((float)step_scale[0] * Math.abs(x_dist)));
         y_feed = (long)(move_will_take / ((float)step_scale[1] * Math.abs(y_dist)));
         z_feed = (long)(move_will_take / ((float)step_scale[2] * Math.abs(z_dist)));
+
+        if (x_feed > 9000000000000000000l) x_feed = 0;
+        if (y_feed > 9000000000000000000l) y_feed = 0;
+        if (z_feed > 9000000000000000000l) z_feed = 0;
         return new long[] {x_feed, y_feed, z_feed};
     }
 
@@ -141,6 +260,33 @@ public class MotionEngine {
         MotionController.WriteBuffer(send_buf);
         move_buffer.add(s);
     }
+    private void pushLinearMove(float[] start_point, float[] end_point, float feedrate)
+    {
+        float x_dist = (end_point[0] - start_point[0]);
+        float y_dist = (end_point[1] - start_point[1]);
+        float z_dist = (end_point[2] - start_point[2]);
+        //System.out.println("x_dist: " + x_dist);
+        //System.out.println("y_dist: " + y_dist);
+        long[] feedrates = getIndividualAxisFeedrates(feedrate, x_dist, y_dist, z_dist, distanceBetween3DPoints(start_point, end_point));
+        StepGenStruct gen = new StepGenStruct();
+        gen.x_step_count = (long)(Math.abs(x_dist) * (float)step_scale[0]);
+        gen.x_total_step_count = gen.x_step_count;
+        gen.x_dir = true;
+        if (x_dist < 0) gen.x_dir = false;
+        gen.y_step_count = (long)(Math.abs(y_dist) * (float)step_scale[1]);
+        gen.y_total_step_count = gen.y_step_count;
+        gen.y_dir = true;
+        if (y_dist < 0) gen.y_dir = false;
+        gen.z_step_count = (long)(Math.abs(z_dist) * (float)step_scale[2]);
+        gen.z_total_step_count = gen.z_step_count;
+        gen.z_dir = true;
+        if (z_dist < 0) gen.z_dir = false;
+        gen.x_delay = feedrates[0];
+        gen.y_delay = feedrates[1];
+        gen.z_delay = feedrates[2];
+        gen.target_velocity = feedrate;
+        pushMoveToStack(gen);
+    }
     public void runMoves()
     {
         move_dro_position = new float[] {GlobalData.dro[0], GlobalData.dro[1], GlobalData.dro[2]};
@@ -148,65 +294,31 @@ public class MotionEngine {
         {
             if (moves.get(x).Gword == 0)
             {
-                float x_dist = (moves.get(x).Xword - move_dro_position[0]);
-                float y_dist = (moves.get(x).Yword - move_dro_position[1]);
-                float z_dist = (moves.get(x).Zword - move_dro_position[2]);
-                //System.out.println("x_dist: " + x_dist);
-                //System.out.println("y_dist: " + y_dist);
-                long[] feedrates = getIndividualAxisFeedrates(max_linear_velocity, x_dist, y_dist, z_dist, distanceBetween3DPoints(move_dro_position, new float[] { moves.get(x).Xword,moves.get(x).Yword, moves.get(x).Zword} ));
-                StepGenStruct gen = new StepGenStruct();
-                gen.x_step_count = (long)(Math.abs(x_dist) * (float)step_scale[0]);
-                gen.x_total_step_count = gen.x_step_count;
-                gen.x_dir = true;
-                if (x_dist < 0) gen.x_dir = false;
-                gen.y_step_count = (long)(Math.abs(y_dist) * (float)step_scale[1]);
-                gen.y_total_step_count = gen.y_step_count;
-                gen.y_dir = true;
-                if (y_dist < 0) gen.y_dir = false;
-                gen.z_step_count = (long)(Math.abs(z_dist) * (float)step_scale[2]);
-                gen.z_total_step_count = gen.z_step_count;
-                gen.z_dir = true;
-                if (z_dist < 0) gen.z_dir = false;
-                gen.x_delay = feedrates[0];
-                gen.y_delay = feedrates[1];
-                gen.z_delay = feedrates[2];
-                gen.target_velocity = max_linear_velocity;
-                pushMoveToStack(gen);
+                pushLinearMove(move_dro_position, new float[] {moves.get(x).Xword, moves.get(x).Yword, moves.get(x).Zword}, max_linear_velocity);
             }
-            if (moves.get(x).Gword == 1 || moves.get(x).Gword == 2 || moves.get(x).Gword == 3)
+            if (moves.get(x).Gword == 1)
             {
-                float x_dist = (moves.get(x).Xword - move_dro_position[0]);
-                float y_dist = (moves.get(x).Yword - move_dro_position[1]);
-                float z_dist = (moves.get(x).Zword - move_dro_position[2]);
-                //System.out.println("x_dist: " + x_dist);
-                //System.out.println("y_dist: " + y_dist);
-                long[] feedrates = getIndividualAxisFeedrates(moves.get(x).Fword, x_dist, y_dist, z_dist, distanceBetween3DPoints(move_dro_position, new float[] { moves.get(x).Xword,moves.get(x).Yword, moves.get(x).Zword} ));
-                StepGenStruct gen = new StepGenStruct();
-                gen.x_step_count = (long)(Math.abs(x_dist) * (float)step_scale[0]);
-                gen.x_total_step_count = gen.x_step_count;
-                gen.x_dir = true;
-                if (x_dist < 0) gen.x_dir = false;
-                gen.y_step_count = (long)(Math.abs(y_dist) * (float)step_scale[1]);
-                gen.y_total_step_count = gen.y_step_count;
-                gen.y_dir = true;
-                if (y_dist < 0) gen.y_dir = false;
-                gen.z_step_count = (long)(Math.abs(z_dist) * (float)step_scale[2]);
-                gen.z_total_step_count = gen.z_step_count;
-                gen.z_dir = true;
-                if (z_dist < 0) gen.z_dir = false;
-                gen.x_delay = feedrates[0];
-                gen.y_delay = feedrates[1];
-                gen.z_delay = feedrates[2];
-                gen.target_velocity = moves.get(x).Fword;
-                pushMoveToStack(gen);
+                pushLinearMove(move_dro_position, new float[] {moves.get(x).Xword, moves.get(x).Yword, moves.get(x).Zword}, moves.get(x).Fword);
             }
             if (moves.get(x).Gword == 2)
             {
-
+                float[] center = new float[]{move_dro_position[0] + moves.get(x).Iword, move_dro_position[1] + moves.get(x).Jword};
+                float radius = new Float(Math.hypot(moves.get(x).Xword-center[0], moves.get(x).Yword-center[1]));
+                ArrayList<float[]> points = getPointsOfArc(move_dro_position, new float[] {moves.get(x).Xword, moves.get(x).Yword }, center, radius, "CW", 5);
+                for (int y = 1; y < points.size(); y++)
+                {
+                    pushLinearMove(new float[] {points.get(y-1)[0], points.get(y-1)[1], 0 }, new float[] {points.get(y)[0], points.get(y)[1], 0 }, moves.get(x).Fword);
+                }
             }
             if (moves.get(x).Gword == 3)
             {
-
+                float[] center = new float[]{move_dro_position[0] + moves.get(x).Iword, move_dro_position[1] + moves.get(x).Jword};
+                float radius = new Float(Math.hypot(moves.get(x).Xword-center[0], moves.get(x).Yword-center[1]));
+                ArrayList<float[]> points = getPointsOfArc(move_dro_position, new float[] {moves.get(x).Xword, moves.get(x).Yword }, center, radius, "CCW", 5);
+                for (int y = 1; y < points.size(); y++)
+                {
+                    pushLinearMove(new float[] {points.get(y-1)[0], points.get(y-1)[1], 0 }, new float[] {points.get(y)[0], points.get(y)[1], 0 }, moves.get(x).Fword);
+                }
             }
             move_dro_position[0] = moves.get(x).Xword;
             move_dro_position[1] = moves.get(x).Yword;
@@ -274,7 +386,7 @@ public class MotionEngine {
                 System.out.println("Next move");
                 next_move();
                 motion_dro_before_move = new float[] {GlobalData.dro[0], GlobalData.dro[1], GlobalData.dro[2]};
-                //GlobalData.ProgrammedFeedrate = move_buffer.get(0).target_velocity;
+                GlobalData.ProgrammedFeedrate = move_buffer.get(0).target_velocity;
             }
             else
             {
