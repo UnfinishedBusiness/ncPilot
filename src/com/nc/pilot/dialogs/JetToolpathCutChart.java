63b4277a0425ea2f7aa57f66b0b968971ebf1645
UnfinishedBusiness <travis.gillin@gmail.com>
2019-03-18 22:15:43 -0400

    Cut chart elements can be moved up and down now

--- src/com/nc/pilot/dialogs/JetToolpathCutChart.java
+++ src/com/nc/pilot/dialogs/JetToolpathCutChart.java
@@ -6,46 +6,38 @@ import com.nc.pilot.lib.GlobalData;
 import javax.swing.*;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class JetToolpathCutChart extends JFrame {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
+    private JButton AddButton;
+    private JButton RemoveButton;
+    private JButton MoveUp;
+    private JButton MoveDown;
     private JTable table1;
     private JScrollPane scrollPane;
 
     public JetToolpathCutChart() {
 
         //headers for the table
+        table1 = new JTable();
         String[] columns = new String[] {
                 "Material", "Consumable", "Amperage", "Kerf Diam.", "Pierce Height", "Pierce Delay", "Cut Height", "Post Delay", "ATHC Voltage", "Feedrate"
         };
-        //actual data for the table in a 2d array
-        /*Object[][] data = new Object[][] {
-                {"1/8 Mild Steel", "45A Finecut", 38, 0.039f, 0.150f, 1.2f, 0.175f, 1, 100, 52},
-                {"1/4 Mild Steel", "45A Finecut", 45, 0.042f, 0.150f, 1.8f, 0.175f, 1, 100, 35},
-                {"3/8 Mild Steel", "45A Finecut", 45, 0.048f, 0.150f, 1.8f, 0.175f, 1, 100, 28},
-        };*/
-
-        Object[][] data = new Object[GlobalData.configData.CutChart.size()][10];
+        //create table with data
+        DefaultTableModel dtm = new DefaultTableModel(0, 0);
+        dtm.setColumnIdentifiers(columns);
+        table1.setModel(dtm);
         for (int x = 0; x < GlobalData.configData.CutChart.size(); x++)
         {
-            data[x][0] = GlobalData.configData.CutChart.get(x).Material;
-            data[x][1] = GlobalData.configData.CutChart.get(x).Consumable;
-            data[x][2] = GlobalData.configData.CutChart.get(x).Amperage;
-            data[x][3] = GlobalData.configData.CutChart.get(x).KerfDiameter;
-            data[x][4] = GlobalData.configData.CutChart.get(x).PierceHeight;
-            data[x][5] = GlobalData.configData.CutChart.get(x).PierceDelay;
-            data[x][6] = GlobalData.configData.CutChart.get(x).CutHeight;
-            data[x][7] = GlobalData.configData.CutChart.get(x).PostDelay;
-            data[x][8] = GlobalData.configData.CutChart.get(x).ATHCVoltage;
-            data[x][9] = GlobalData.configData.CutChart.get(x).Feedrate;
+            dtm.addRow(new Object[]{GlobalData.configData.CutChart.get(x).Material, GlobalData.configData.CutChart.get(x).Consumable, GlobalData.configData.CutChart.get(x).Amperage, GlobalData.configData.CutChart.get(x).KerfDiameter, GlobalData.configData.CutChart.get(x).PierceHeight, GlobalData.configData.CutChart.get(x).PierceDelay, GlobalData.configData.CutChart.get(x).CutHeight, GlobalData.configData.CutChart.get(x).PostDelay, GlobalData.configData.CutChart.get(x).ATHCVoltage, GlobalData.configData.CutChart.get(x).Feedrate});
         }
-        //create table with data
-        table1 = new JTable(data, columns);
         table1.setPreferredScrollableViewportSize(table1.getPreferredSize());
         table1.setFillsViewportHeight(true);
         table1.getModel().addTableModelListener(new TableModelListener() {
@@ -87,6 +79,22 @@ public class JetToolpathCutChart extends JFrame {
         buttonCancel = new JButton();
         buttonCancel.setText("Cancel");
 
+        AddButton = new JButton();
+        AddButton.setText("Add");
+
+        RemoveButton = new JButton();
+        RemoveButton.setText("Remove");
+
+        MoveUp = new JButton();
+        MoveUp.setText("Move Up");
+
+        MoveDown = new JButton();
+        MoveDown.setText("Move Down");
+
+        contentPane.add(MoveUp);
+        contentPane.add(MoveDown);
+        contentPane.add(AddButton);
+        contentPane.add(RemoveButton);
         contentPane.add(buttonCancel);
         contentPane.add(buttonOK);
 
@@ -102,6 +110,115 @@ public class JetToolpathCutChart extends JFrame {
         buttonCancel.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 onCancel();
+            }
+        });
+
+        AddButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JetToolpathCutChartData cut = new JetToolpathCutChartData();
+                cut.Material = "New Material";
+                cut.Consumable = "45A Finecut";
+                cut.Amperage = 38;
+                cut.KerfDiameter = 0.039f;
+                cut.PierceHeight = 0.150f;
+                cut.PierceDelay = 1.2f;
+                cut.CutHeight = 0.175f;
+                cut.PostDelay = 1f;
+                cut.ATHCVoltage = 100;
+                cut.Feedrate = 52f;
+                GlobalData.configData.CutChart.add(cut);
+                dtm.addRow(new Object[]{cut.Material, cut.Consumable, cut.Amperage, cut.KerfDiameter, cut.PierceHeight, cut.PierceDelay, cut.CutHeight, cut.PostDelay, cut.ATHCVoltage, cut.Feedrate});
+                try {
+                    GlobalData.pushConfig();
+                } catch (FileNotFoundException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+        RemoveButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int row = table1.getSelectedRow();
+                //System.out.println("Selected row: " + row);
+                if (row > 0)
+                {
+                    ArrayList<JetToolpathCutChartData> tmp = new ArrayList();
+                    for (int x = 0; x < GlobalData.configData.CutChart.size(); x++)
+                    {
+                        if (x != row)
+                        {
+                            tmp.add(GlobalData.configData.CutChart.get(x));
+                        }
+                    }
+                    GlobalData.configData.CutChart = tmp;
+                    dtm.removeRow(row);
+                    try {
+                        GlobalData.pushConfig();
+                    } catch (FileNotFoundException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+        MoveUp.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int row = table1.getSelectedRow();
+                //System.out.println("Selected row: " + row);
+                if (row > 0)
+                {
+                    dtm.moveRow(row, row, row-1);
+                    GlobalData.configData.CutChart = new ArrayList();
+                    for (int x = 0; x < dtm.getRowCount(); x++)
+                    {
+                        JetToolpathCutChartData cut = new JetToolpathCutChartData();
+                        cut.Material = dtm.getValueAt(x, 0).toString();
+                        cut.Consumable = dtm.getValueAt(x, 1).toString();
+                        cut.Amperage = new Integer(dtm.getValueAt(x, 2).toString());
+                        cut.KerfDiameter = new Float(dtm.getValueAt(x, 3).toString());;
+                        cut.PierceHeight = new Float(dtm.getValueAt(x, 4).toString());;
+                        cut.PierceDelay = new Float(dtm.getValueAt(x, 5).toString());;
+                        cut.CutHeight = new Float(dtm.getValueAt(x, 6).toString());;
+                        cut.PostDelay = new Float(dtm.getValueAt(x, 7).toString());;
+                        cut.ATHCVoltage = new Integer(dtm.getValueAt(x, 8).toString());;
+                        cut.Feedrate = new Float(dtm.getValueAt(x, 9).toString());;
+                        GlobalData.configData.CutChart.add(cut);
+                    }
+                    try {
+                        GlobalData.pushConfig();
+                    } catch (FileNotFoundException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+        MoveDown.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int row = table1.getSelectedRow();
+                //System.out.println("Selected row: " + row);
+                if (row > 0)
+                {
+                    dtm.moveRow(row, row, row+1);
+                    GlobalData.configData.CutChart = new ArrayList();
+                    for (int x = 0; x < dtm.getRowCount(); x++)
+                    {
+                        JetToolpathCutChartData cut = new JetToolpathCutChartData();
+                        cut.Material = dtm.getValueAt(x, 0).toString();
+                        cut.Consumable = dtm.getValueAt(x, 1).toString();
+                        cut.Amperage = new Integer(dtm.getValueAt(x, 2).toString());
+                        cut.KerfDiameter = new Float(dtm.getValueAt(x, 3).toString());;
+                        cut.PierceHeight = new Float(dtm.getValueAt(x, 4).toString());;
+                        cut.PierceDelay = new Float(dtm.getValueAt(x, 5).toString());;
+                        cut.CutHeight = new Float(dtm.getValueAt(x, 6).toString());;
+                        cut.PostDelay = new Float(dtm.getValueAt(x, 7).toString());;
+                        cut.ATHCVoltage = new Integer(dtm.getValueAt(x, 8).toString());;
+                        cut.Feedrate = new Float(dtm.getValueAt(x, 9).toString());;
+                        GlobalData.configData.CutChart.add(cut);
+                    }
+                    try {
+                        GlobalData.pushConfig();
+                    } catch (FileNotFoundException e1) {
+                        e1.printStackTrace();
+                    }
+                }
             }
         });
 
