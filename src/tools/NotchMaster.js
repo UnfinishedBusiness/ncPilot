ca59180af2027d6806b3755077bf67b4322a8c26
UnfinishedBusiness <travis.gillin@gmail.com>
2020-01-06 11:42:32 -0500

    NotchMaster operation selectors work now

--- src/tools/NotchMaster.js
+++ src/tools/NotchMaster.js
@@ -144,29 +144,57 @@ NotchMaster.ProcessNotches = function()
 	//Draw a flat visual of our base tube
 	var tube_diameter = gui.get_input_double(this.dialog.id, this.dialog.tube_diameter);
 	var tube_length = gui.get_input_double(this.dialog.id, this.dialog.tube_length);
-	render.add_entity({ type: "line", start: {x: 0 + MotionControl.machine_parameters.work_offset.x, y: 0 + MotionControl.machine_parameters.work_offset.y}, end: {x: (tube_diameter * 3.14) + MotionControl.machine_parameters.work_offset.x, y: 0 + MotionControl.machine_parameters.work_offset.y}, color: { r: 0, g: 1, b: 0} });
-	render.add_entity({ type: "line", start: {x: (tube_diameter * 3.14) + MotionControl.machine_parameters.work_offset.x, y: 0 + MotionControl.machine_parameters.work_offset.y}, end: {x: (tube_diameter * 3.14) + MotionControl.machine_parameters.work_offset.x, y: (tube_length * -1) + MotionControl.machine_parameters.work_offset.y}, color: { r: 0, g: 1, b: 0} });
-	render.add_entity({ type: "line", start: {x: (tube_diameter * 3.14) + MotionControl.machine_parameters.work_offset.x, y: (tube_length * -1) + MotionControl.machine_parameters.work_offset.y}, end: {x: 0 + MotionControl.machine_parameters.work_offset.x, y: (tube_length * -1) + MotionControl.machine_parameters.work_offset.y}, color: { r: 0, g: 1, b: 0} });
-	render.add_entity({ type: "line", start: {x: 0 + MotionControl.machine_parameters.work_offset.x, y: (tube_length * -1) + MotionControl.machine_parameters.work_offset.y}, end: {x: 0 + MotionControl.machine_parameters.work_offset.x, y: 0 + MotionControl.machine_parameters.work_offset.y}, color: { r: 0, g: 1, b: 0} });
-	//Draw side one notch
-	var notch_one = this.CalculateNotch(gui.get_input_double(this.dialog.id, this.dialog.tube_diameter), gui.get_input_double(this.dialog.id, this.dialog.side_one_header_diameter), gui.get_input_double(this.dialog.id, this.dialog.wall_thickness), gui.get_input_double(this.dialog.id, this.dialog.side_one_mate_angle), true);
-	var last_pointer = { x: notch_one[0].x, y: notch_one[0].y };
-	for (var x = 1; x < notch_one.length; x++)
+	
+	var side_one_operation = gui.get_radiogroup(this.dialog.id, this.dialog.side_one_operation);
+	if (side_one_operation == 0) //Notch Side 1
 	{
-		render.add_entity({ type: "line", start: {x: last_pointer.x + MotionControl.machine_parameters.work_offset.x, y: last_pointer.y + MotionControl.machine_parameters.work_offset.y}, end: {x: notch_one[x].x + MotionControl.machine_parameters.work_offset.x, y: notch_one[x].y + MotionControl.machine_parameters.work_offset.y}, color: { r: 1, g: 1, b: 1} });
-		last_pointer = { x: notch_one[x].x, y: notch_one[x].y };
+		//Draw side one notch
+		var notch_one = this.CalculateNotch(gui.get_input_double(this.dialog.id, this.dialog.tube_diameter), gui.get_input_double(this.dialog.id, this.dialog.side_one_header_diameter), gui.get_input_double(this.dialog.id, this.dialog.wall_thickness), gui.get_input_double(this.dialog.id, this.dialog.side_one_mate_angle), true);
+		var last_pointer = { x: notch_one[0].x, y: notch_one[0].y };
+		for (var x = 1; x < notch_one.length; x++)
+		{
+			render.add_entity({ type: "line", start: {x: last_pointer.x + MotionControl.machine_parameters.work_offset.x, y: last_pointer.y + MotionControl.machine_parameters.work_offset.y}, end: {x: notch_one[x].x + MotionControl.machine_parameters.work_offset.x, y: notch_one[x].y + MotionControl.machine_parameters.work_offset.y}, color: { r: 1, g: 1, b: 1} });
+			last_pointer = { x: notch_one[x].x, y: notch_one[x].y };
+		}
+	}
+	else if (side_one_operation == 1) //Slice Side 1
+	{
+		//Draw side one slice
+		render.add_entity({ type: "line", start: {x: 0 + MotionControl.machine_parameters.work_offset.x, y: 0 + MotionControl.machine_parameters.work_offset.y}, end: {x: (tube_diameter * 3.14) + MotionControl.machine_parameters.work_offset.x, y: 0 + MotionControl.machine_parameters.work_offset.y}, color: { r: 0, g: 0, b: 0} });
+	}
+	
+	
+	var side_two_operation = gui.get_radiogroup(this.dialog.id, this.dialog.side_two_operation);
+	if (side_two_operation == 0) //Notch Side 2
+	{
+		//Draw side two notch
+		var notch_two = this.CalculateNotch(gui.get_input_double(this.dialog.id, this.dialog.tube_diameter), gui.get_input_double(this.dialog.id, this.dialog.side_two_header_diameter), gui.get_input_double(this.dialog.id, this.dialog.wall_thickness), gui.get_input_double(this.dialog.id, this.dialog.side_two_mate_angle), false);
+		for (var x = 0; x < notch_two.length; x++)
+		{
+			notch_two[x].y -= tube_length;
+		}
+		var last_pointer = { x: notch_two[0].x, y: notch_two[0].y };
+		for (var x = 1; x < notch_two.length; x++)
+		{
+			render.add_entity({ type: "line", start: {x: last_pointer.x + MotionControl.machine_parameters.work_offset.x, y: last_pointer.y + MotionControl.machine_parameters.work_offset.y}, end: {x: notch_two[x].x + MotionControl.machine_parameters.work_offset.x, y: notch_two[x].y + MotionControl.machine_parameters.work_offset.y}, color: { r: 1, g: 1, b: 1} });
+			last_pointer = { x: notch_two[x].x, y: notch_two[x].y };
+		}
+	}
+	else if (side_two_operation == 1) //Slice Side 2
+	{
+		//Draw side two slice
+		render.add_entity({ type: "line", start: {x: 0 + MotionControl.machine_parameters.work_offset.x, y: (tube_length * -1) + MotionControl.machine_parameters.work_offset.y}, end: {x: (tube_diameter * 3.14) + MotionControl.machine_parameters.work_offset.x, y: (tube_length * -1) + MotionControl.machine_parameters.work_offset.y}, color: { r: 1, g: 1, b: 1} });
+	}
+	else if (side_two_operation == 2) //Disable Side 2
+	{
+
 	}
 
-	//Draw side two notch
-	var notch_two = this.CalculateNotch(gui.get_input_double(this.dialog.id, this.dialog.tube_diameter), gui.get_input_double(this.dialog.id, this.dialog.side_two_header_diameter), gui.get_input_double(this.dialog.id, this.dialog.wall_thickness), gui.get_input_double(this.dialog.id, this.dialog.side_two_mate_angle), false);
-	for (var x = 0; x < notch_two.length; x++)
+	if ((side_one_operation == 0 || side_one_operation == 1) && (side_two_operation == 0 || side_two_operation == 1))
 	{
-		notch_two[x].y -= tube_length;
-	}
-	var last_pointer = { x: notch_two[0].x, y: notch_two[0].y };
-	for (var x = 1; x < notch_one.length; x++)
-	{
-		render.add_entity({ type: "line", start: {x: last_pointer.x + MotionControl.machine_parameters.work_offset.x, y: last_pointer.y + MotionControl.machine_parameters.work_offset.y}, end: {x: notch_two[x].x + MotionControl.machine_parameters.work_offset.x, y: notch_two[x].y + MotionControl.machine_parameters.work_offset.y}, color: { r: 1, g: 1, b: 1} });
-		last_pointer = { x: notch_two[x].x, y: notch_two[x].y };
+		//render.add_entity({ type: "line", start: {x: 0 + MotionControl.machine_parameters.work_offset.x, y: 0 + MotionControl.machine_parameters.work_offset.y}, end: {x: (tube_diameter * 3.14) + MotionControl.machine_parameters.work_offset.x, y: 0 + MotionControl.machine_parameters.work_offset.y}, color: { r: 0, g: 1, b: 0} });
+		render.add_entity({ type: "line", start: {x: (tube_diameter * 3.14) + MotionControl.machine_parameters.work_offset.x, y: 0 + MotionControl.machine_parameters.work_offset.y}, end: {x: (tube_diameter * 3.14) + MotionControl.machine_parameters.work_offset.x, y: (tube_length * -1) + MotionControl.machine_parameters.work_offset.y}, color: { r: 0, g: 1, b: 0} });
+		//render.add_entity({ type: "line", start: {x: (tube_diameter * 3.14) + MotionControl.machine_parameters.work_offset.x, y: (tube_length * -1) + MotionControl.machine_parameters.work_offset.y}, end: {x: 0 + MotionControl.machine_parameters.work_offset.x, y: (tube_length * -1) + MotionControl.machine_parameters.work_offset.y}, color: { r: 0, g: 1, b: 0} });
+		render.add_entity({ type: "line", start: {x: 0 + MotionControl.machine_parameters.work_offset.x, y: (tube_length * -1) + MotionControl.machine_parameters.work_offset.y}, end: {x: 0 + MotionControl.machine_parameters.work_offset.x, y: 0 + MotionControl.machine_parameters.work_offset.y}, color: { r: 0, g: 1, b: 0} });
 	}
 }
