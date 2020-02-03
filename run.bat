fef72a6bec24c8ce37c27baa54dcbcfeb2f868ff
UnfinishedBusiness <travis.gillin@gmail.com>
2020-02-03 13:51:15 -0400

    Updated windows run script

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @echo off
-c:\msys64\msys2_shell.cmd -mingw64 -no-start -defterm -c "cd /c/Xmotion/ncPilot && ./Xkernel/Xkernel ./src/main.js"
+c:\msys64\msys2_shell.cmd -mingw64 -no-start -defterm -where "C:\Xmotion\ncPilot" -c "../Xkernel/Xkernel ./src/main.js"
