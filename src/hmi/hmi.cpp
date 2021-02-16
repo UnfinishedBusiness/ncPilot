5f9c3345352bc2f605890d02dece3ce0e4669f24
Travis Gillin <travis.gillin@gmail.com>
2021-02-16 15:23:32 -0500

    HMI is pretty much sorted out

--- src/hmi/hmi.cpp
+++ src/hmi/hmi.cpp
@@ -7,7 +7,11 @@
 
 double hmi_backplane_width = 300;
 Xrender_object_t *hmi_backpane;
+double hmi_dro_backplane_height = 200;
 Xrender_object_t *hmi_dro_backpane;
+Xrender_object_t *hmi_button_backpane;
+
+std::vector<hmi_button_group_t> button_groups;
 
 void mouse_callback(Xrender_object_t* o,nlohmann::json e)
 {
@@ -15,15 +19,23 @@ void mouse_callback(Xrender_object_t* o,nlohmann::json e)
     {
         if (e["event"] == "mouse_in")
         {
-            
+            //printf("Mouse In - %s\n", e.dump().c_str());
+            o->data["color"] = {{"r", 10}, {"g", 100}, {"b", 10}, {"a", 255}};
         }
         if (e["event"] == "mouse_out")
         {
-            
+            //printf("Mouse Out - %s\n", e.dump().c_str());
+            o->data["color"] = {{"r", 10}, {"g", 10}, {"b", 10}, {"a", 255}};
         }
         if (e["event"] == "left_click_down")
         {
-            
+            //printf("Mouse Down - %s\n", e.dump().c_str());
+            o->data["color"] = {{"r", 10}, {"g", 150}, {"b", 10}, {"a", 255}};
+        }
+        if (e["event"] == "left_click_up")
+        {
+            //printf("Mouse Up - %s\n", e.dump().c_str());
+            o->data["color"] = {{"r", 10}, {"g", 100}, {"b", 10}, {"a", 255}};
         }
     }
 }
@@ -59,8 +71,117 @@ bool hmi_update_timer()
     hmi_backpane->data["br"] = {{"x", ((double)globals->Xcore->data["window_width"] / 2)}, {"y", ((double)globals->Xcore->data["window_height"] / 2)}};
 
     hmi_dro_backpane->data["tl"] = {{"x", (((double)globals->Xcore->data["window_width"] / 2) - hmi_backplane_width) + 5}, {"y", ((double)globals->Xcore->data["window_height"] / 2) - 25}};
-    hmi_dro_backpane->data["br"] = {{"x", ((double)globals->Xcore->data["window_width"] / 2) - 5}, {"y", (((double)globals->Xcore->data["window_height"] / 2) - 25 - 200)}};
+    hmi_dro_backpane->data["br"] = {{"x", ((double)globals->Xcore->data["window_width"] / 2) - 5}, {"y", (((double)globals->Xcore->data["window_height"] / 2) - 25 - hmi_dro_backplane_height)}};
+
+    hmi_button_backpane->data["tl"] = {{"x", ((double)globals->Xcore->data["window_width"] / 2) - hmi_backplane_width + 5}, {"y", (((double)globals->Xcore->data["window_height"] / 2) - 25 - hmi_dro_backplane_height - 10)}};
+    hmi_button_backpane->data["br"] = {{"x", ((double)globals->Xcore->data["window_width"] / 2) - 5}, {"y", -((double)globals->Xcore->data["window_height"] / 2) + 5}};
+
+    double button_group_x = (double)hmi_button_backpane->data["tl"]["x"];
+    double button_group_y = (double)hmi_button_backpane->data["tl"]["y"];
+    double button_height = (button_group_y - (double)hmi_button_backpane->data["br"]["y"]) / (double)button_groups.size();
+    double button_width = ((double)hmi_button_backpane->data["br"]["x"] - (double)hmi_button_backpane->data["tl"]["x"]) / 2;
+    
+    //printf("button_group_x: %.4f, button_group_y: %.4f, button_width: %.4f, button_height: %.4f\n", button_group_x, button_group_y, button_width, button_height);
+    double center_x;
+    double center_y;
+    for (int x = 0; x < button_groups.size(); x++)
+    {
+        button_groups[x].button_one.object->data["tl"] = {{"x", button_group_x + 5}, {"y", button_group_y - 2.5}};
+        button_groups[x].button_one.object->data["br"] = {{"x", (button_group_x - 5) + button_width}, {"y", (button_group_y + 2.5) - button_height}};
+        center_x = (double)button_groups[x].button_one.object->data["tl"]["x"] + (button_width / 2);
+        center_y = (double)button_groups[x].button_one.object->data["tl"]["y"] - (button_height / 2);
+        button_groups[x].button_one.label->data["position"] = {{"x", center_x - ((double)button_groups[x].button_one.label->data["size"]["width"] / 2.0f) - 5}, {"y", center_y - ((double)button_groups[x].button_one.label->data["size"]["height"] / 2.0f) + 5}};
+
+        button_group_x += button_width;
+        button_groups[x].button_two.object->data["tl"] = {{"x", button_group_x + 5}, {"y", button_group_y - 2.5}};
+        button_groups[x].button_two.object->data["br"] = {{"x", (button_group_x - 5) + button_width}, {"y", (button_group_y + 2.5) - button_height}};
+        center_x = (double)button_groups[x].button_two.object->data["tl"]["x"] + (button_width / 2);
+        center_y = (double)button_groups[x].button_two.object->data["tl"]["y"] - (button_height / 2);
+        button_groups[x].button_two.label->data["position"] = {{"x", center_x - ((double)button_groups[x].button_two.label->data["size"]["width"] / 2.0f) - 5}, {"y", center_y - ((double)button_groups[x].button_two.label->data["size"]["height"] / 2.0f) + 5}};
+        button_group_x -= button_width;
+
+        button_group_y -= button_height;
+    }
     return true;
+}
+void hmi_push_button_group(std::string b1, std::string b2)
+{
+    hmi_button_group_t group;
+    group.button_one.name = b1;
+    group.button_one.object = Xrender_push_box({
+        {"tl", {
+            {"x", -100000},
+            {"y", -100000}
+        }},
+        {"br", {
+            {"x", -100000},
+            {"y", -100000}
+        }},
+        {"radius", 5},
+        {"zindex", 200},
+        {"color", {
+            {"r", 10},
+            {"g", 10},
+            {"b", 10},
+            {"a", 255}
+        }},
+    });
+    group.button_one.label = Xrender_push_text({
+        {"textval", group.button_one.name},
+        {"font", "default"},
+        {"position", {
+            {"x", -10000},
+            {"y", -10000}
+        }},
+        {"font_size", 20},
+        {"zindex", 210},
+        {"angle", 0},
+        {"color", {
+            {"r", 255},
+            {"g", 255},
+            {"b", 255},
+            {"a", 255},
+        }},
+    });
+    group.button_two.name = b2;
+    group.button_two.object = Xrender_push_box({
+        {"tl", {
+            {"x", -100000},
+            {"y", -100000}
+        }},
+        {"br", {
+            {"x", -100000},
+            {"y", -100000}
+        }},
+        {"radius", 5},
+        {"zindex", 200},
+        {"color", {
+            {"r", 10},
+            {"g", 10},
+            {"b", 10},
+            {"a", 255}
+        }},
+    });
+    group.button_two.label = Xrender_push_text({
+        {"textval", group.button_two.name},
+        {"font", "default"},
+        {"position", {
+            {"x", -10000},
+            {"y", -10000}
+        }},
+        {"font_size", 20},
+        {"zindex", 210},
+        {"angle", 0},
+        {"color", {
+            {"r", 255},
+            {"g", 255},
+            {"b", 255},
+            {"a", 255},
+        }},
+    });
+    group.button_one.object->mouse_callback = mouse_callback;
+    group.button_two.object->mouse_callback = mouse_callback;
+    button_groups.push_back(group);
 }
 void hmi_init()
 {
@@ -140,5 +261,32 @@ void hmi_init()
             {"a", 255}
         }},
     });
+    hmi_button_backpane = Xrender_push_box({
+        {"tl", {
+            {"x", -100000},
+            {"y", -100000}
+        }},
+        {"br", {
+            {"x", -100000},
+            {"y", -100000}
+        }},
+        {"radius", 5},
+        {"zindex", 100},
+        {"color", {
+            {"r", 29},
+            {"g", 32},
+            {"b", 48},
+            {"a", 255}
+        }},
+    });
+
+    hmi_push_button_group("Zero X", "Zero Y");
+    hmi_push_button_group("Touch", "Retract");
+    hmi_push_button_group("Touch", "Retract");
+    hmi_push_button_group("Fit", "Clean");
+    hmi_push_button_group("Wpos", "Park");
+    hmi_push_button_group("Test Run", "ATHC");
+    hmi_push_button_group("Run", "Abort");
+
     Xrender_push_timer(100, hmi_update_timer);
 }
--- src/hmi/hmi.h
+++ src/hmi/hmi.h
@@ -1,6 +1,17 @@
 #ifndef HMI_
 #define HMI_
 #include <Xrender.h>
+#include <string>
+
+struct hmi_button_t{
+    std::string name;
+    Xrender_object_t *object;
+    Xrender_object_t *label;
+};
+struct hmi_button_group_t{
+    hmi_button_t button_one;
+    hmi_button_t button_two;
+};
 
 void hmi_init();
 
